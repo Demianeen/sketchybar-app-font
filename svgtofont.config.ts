82023ad82b495fd7b65b8986e202b987c710e58b
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2025-01-05 19:46:04 +0000

    feat: remove mappings

---
+++ svgtofont.config.ts
@@ -0,0 +1,19 @@
+/**
+ * @type {import('svgtofont').SvgToFontOptions}
+ */
+export default {
+  fontSize: "12px",
+  fontName: "sketchybar-app-font",
+  website: false,
+  css: false,
+  emptyDist: true,
+  useNameAsUnicode: true,
+  svgicons2svgfont: {
+    centerhorizontally: true,
+    centervertically: true,
+    height: 16,
+    normalize: true,
+    preserveAspectRatio: true,
+    fixedWidth: true,
+  },
+};
